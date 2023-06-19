13cb6736b3c173ed2441e959e2690d3762d5c557
yaminichaudhari2001 <130443234+yaminichaudhari2001@users.noreply.github.com>
2023-06-19 12:25:00 +0530

    Update 1 Season.java

--- springseason/src/print/spring/season/Season.java
+++ springseason/src/print/spring/season/Season.java
@@ -1,6 +1,6 @@
 //. Write a program SpringSeason.java that
 // takes two int values m and d from the command line and prints true if day d of month m is
-// between March 20 (m = 3, d=20) and June 20 (m = 6, d = 20), false otherwise.
+// between March 20 (m = 3, d=20) and June 20 (m =6, d = 20), false otherwise.
 package print.spring.season;
 import java.util.*;
 
